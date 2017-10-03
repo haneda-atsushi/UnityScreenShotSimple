e01983555603b720b460706eef844897531f1716
haneda-atsushi <hanecci@gmail.com>
2017-10-04 01:27:34 +0900

    Initial commit

---
+++ Assets/Scripts/ScreenShotController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class ScreenShotController : MonoBehaviour
+{
+    public KeyCode m_ScreenShotButton = KeyCode.F9;
+
+    const string m_DefaultDir = "Screenshots";
+    string       m_FilePath   = null;
+
+    public void TriggerCapture( string file_path )
+    {
+        m_FilePath = file_path;
+        StartCoroutine( SaveScreenAsPNG() );
+    }
+
+    void Update()
+    {
+        if ( Input.GetKeyDown( m_ScreenShotButton ) )
+        {
+            m_FilePath = null;
+
+            StartCoroutine( SaveScreenAsPNG() );
+        }
+    }
+
+    // https://docs.unity3d.com/ScriptReference/WWWForm.html
+    IEnumerator SaveScreenAsPNG()
+    {
+        yield return new WaitForEndOfFrame();
+
+        int width = Screen.width;
+        int height = Screen.height;
+        var tex = new Texture2D( width, height, TextureFormat.RGB24, false );
+
+        tex.ReadPixels( new Rect(0, 0, width, height), 0, 0 );
+        tex.Apply();
+
+        byte[] bytes = tex.EncodeToPNG();
+
+        string file_path = m_FilePath;
+        if ( m_FilePath == null )
+        {
+            file_path = GetScreenShotFilePath();
+            TryCreateScreenShotDir();
+        }
+
+        File.WriteAllBytes( file_path, bytes );
+
+        Destroy( tex );
+    }
+
+    public void TryCreateScreenShotDir()
+    {
+        if ( ! Directory.Exists( m_DefaultDir ) )
+        {
+            Directory.CreateDirectory( m_DefaultDir );
+        }
+    }
+
+    public string GetScreenShotFilePath()
+    {
+        System.DateTime this_day = System.DateTime.Now;
+        string time_str = 
+            string.Format( "{0}{1}{2}_{3}_{4}_{5}",
+	                        this_day.Year, this_day.Month, this_day.Day,
+                            this_day.Hour, this_day.Minute, this_day.Second );
+        string filename = string.Format( "{0}/Screenshot_{1}.png", m_DefaultDir, time_str );
+
+        return filename;
+    }
+}
---
+++ Assets/Scripts/TestScreenShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class TestScreenShot : MonoBehaviour
+{
+    ScreenShotController m_ScreenShotController;
+
+	void Start ()
+    {
+        m_ScreenShotController = FindObjectOfType<ScreenShotController>();
+        Assert.IsNotNull( m_ScreenShotController );
+
+        m_ScreenShotController.TryCreateScreenShotDir();
+    }
+
+    void OnGUI()
+    {
+        if ( GUI.Button( new Rect( 100, 100, 200, 100 ), "Take screenshot" ) )
+        {
+            string file_path = m_ScreenShotController.GetScreenShotFilePath();
+            m_ScreenShotController.TriggerCapture( file_path );
+
+            Debug.LogFormat( "Screenshot is saved to {0}.", file_path );
+        }
+    }
+}
